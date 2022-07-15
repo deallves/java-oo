22bfe0437bce544c461f4bc62c0788883b535de7
deallves <deallves@gmail.com>
2022-07-14 21:39:46 -0300

    metodo this void

--- bytbank/src/TesteConta1.java
+++ bytbank/src/TesteConta1.java
@@ -1,13 +1,14 @@
 
 public class TesteConta1 {
-	public class Teste {
+	
 		public static void main(String[] args) {
 			Conta minhaConta = new Conta();
 			minhaConta.saldo = 500.0;
 			Conta outraConta = minhaConta;
 			outraConta.saldo += 1000.0;
 			System.out.println(minhaConta.saldo);
+			
 		}
 	}
 
-}
+
---
+++ bytbank/src/console.java
@@ -0,0 +1,17 @@
+
+public class console {
+	public static void main(String[] args) {
+		
+
+		console.log("\uD83D\uDC99 💙️");
+
+		System.out.println("\uD83D\uDC99 💙️");
+
+	}
+
+	private static void log(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
