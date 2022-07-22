81a38f29bc2e9fdce2c73eb9c9a1f83c50929df9
deallves <deallves@gmail.com>
2022-07-21 22:13:58 -0300

    metodos getters e setters

--- bytebank-encapisulado/src/Conta.java
+++ bytebank-encapisulado/src/Conta.java
@@ -1,11 +1,10 @@
 public class Conta {
-	private double saldo ;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 
 	public void deposita(double valor) {
-
 		this.saldo += valor;
 
 	}
@@ -24,15 +23,36 @@ public class Conta {
 			this.saldo -= valor;
 			destino.deposita(valor);
 			return true;
-		} 
-			return false;
-			
 		}
-	
+		return false;
+
+	}
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
 	}
 
-
+}
