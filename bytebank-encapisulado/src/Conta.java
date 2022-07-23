23e4954ccf2fd4cd4f9a9c621de9cc0c569a3d74
deallves <deallves@gmail.com>
2022-07-22 22:18:28 -0300

    encapisulamento, private e static

--- bytebank-encapisulado/src/Conta.java
+++ bytebank-encapisulado/src/Conta.java
@@ -3,6 +3,19 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total;
+	
+	
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		//System.out.println("o total de contas é " + Conta.total);
+		
+		this.agencia = agencia;
+		this.numero = numero;
+		//System.out.println("estou criando uma conta"+ this.numero);
+		
+	}
 
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -36,6 +49,9 @@ public class Conta {
 	}
 	
 	public void setNumero(int numero) {
+		if(numero <= 0 ) {
+			return;
+		}
 		this.numero = numero;
 	}
 	
@@ -44,6 +60,11 @@ public class Conta {
 	}
 	
 	public void setAgencia(int agencia) {
+		if(agencia<= 0) {
+			System.out.println("nao pode valor menos igual a 0");
+			return;
+		}
+		
 		this.agencia = agencia;
 	}
 	
@@ -53,6 +74,11 @@ public class Conta {
 	
 	public Cliente getTitular() {
 		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+		
 	}
 
 }
